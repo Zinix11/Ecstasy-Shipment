5b040484ad038d163e4acc37ca6a63bda1686129
Zinix <45179619+Zinix11@users.noreply.github.com>
2018-11-22 06:42:17 +1000

    Update config.lua

--- esx_ecstasy_shipment/config.lua
+++ esx_ecstasy_shipment/config.lua
@@ -6,7 +6,7 @@ Config.RequiredCopsSell = 1
 
 Stores = {
 	["Shipment"] = {
-		position = { ['x'] = -629.99, ['y'] = -236.542, ['z'] = 38.05 },       
+		position = { ['x'] = 32.89, ['y'] = -2690.678, ['z'] = 6.008 },       
 		reward = math.random(100000,150000),
 		nameofstore = "shipment",
 		lastrobbed = 0
